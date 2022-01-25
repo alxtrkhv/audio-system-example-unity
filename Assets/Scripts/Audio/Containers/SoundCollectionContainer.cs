0f2e586109da59821c016d5b02cef4851e5241c3
Alexander Terekhov <alxtrkhv@gmail.com>
2022-01-25 19:28:56 +0300

    feat: add type dropdown for sound collection

--- Assets/Scripts/Audio/Containers/SoundCollectionContainer.cs
+++ Assets/Scripts/Audio/Containers/SoundCollectionContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,27 @@ namespace Alxtrkhv.AudioSystem
     [CreateAssetMenu(fileName = "New Sound Collection Container", menuName = "Audio/Sound Collection Container")]
     public class SoundCollectionContainer : BaseSoundContainer
     {
+        private enum CollectionMode
+        {
+            Random,
+            Sequential,
+            Switch
+        }
+
         [SerializeField]
-        private bool randomOrder;
+        private CollectionMode collectionMode;
 
         [Header(AudioClipsHeader)]
         [SerializeField]
         private List<SoundContainerMember> members;
 
-        public override SoundContainerType ContainerType => randomOrder ? SoundContainerType.Random : SoundContainerType.Sequential;
+        public override SoundContainerType ContainerType => collectionMode switch
+        {
+            CollectionMode.Random => SoundContainerType.Random,
+            CollectionMode.Sequential => SoundContainerType.Sequential,
+            CollectionMode.Switch => SoundContainerType.Switch,
+            _ => throw new NotImplementedException($"{collectionMode} mode of {nameof(SoundCollectionContainer)} is not supported.")
+        };
 
         public override int Count => members.Count;
 
