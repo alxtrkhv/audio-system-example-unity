744e9c1dd833032e87831542d67c784a607c9f76
Alexander Terekhov <alxtrkhv@gmail.com>
2022-01-25 19:28:56 +0300

    fix: get rid of unwanted an allocation

--- Assets/Scripts/Audio/Containers/BaseSoundContainer.cs
+++ Assets/Scripts/Audio/Containers/BaseSoundContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,14 +13,21 @@ namespace Alxtrkhv.AudioSystem
         [SerializeField]
         private SoundConfig config;
 
+        private string id;
+
         public abstract SoundContainerMember this[int index] { get; }
         public abstract IEnumerator<SoundContainerMember> GetEnumerator();
         public abstract int Count { get; }
 
         public abstract SoundContainerType ContainerType { get; }
 
-        public string Id => name;
+        public string Id => id;
         public SoundConfig Config => config;
+
+        private void OnEnable()
+        {
+            id = name;
+        }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
