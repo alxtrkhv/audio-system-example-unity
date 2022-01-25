bcfdfd18dd56aa060eb023fe0494f88d0e9efe54
Alexander Terekhov <alxtrkhv@gmail.com>
2022-01-25 19:28:55 +0300

    feat: create dummy RangedWeaponImpactSoundController

---
+++ Assets/Scripts/Audio/RangedWeaponImpactSoundController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Alxtrkhv.AudioSystem
+{
+    public class RangedWeaponImpactSoundController : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField]
+        private SoundEventEmitter emitter;
+
+        public void PlatImpactSound(Vector3 position)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UnitSide.cs
@@ -0,0 +1,9 @@
+namespace Alxtrkhv.AudioSystem
+{
+    public enum UnitSide
+    {
+        Enemy,
+        Ally,
+        Player
+    }
+}
