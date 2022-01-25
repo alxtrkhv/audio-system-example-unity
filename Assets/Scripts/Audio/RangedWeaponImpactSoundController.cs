573ef2c54b684c4b73ffa69056d90e3ff6f33246
Alexander Terekhov <alxtrkhv@gmail.com>
2022-01-25 19:28:55 +0300

    feat: implement PlayImpactSound

--- Assets/Scripts/Audio/RangedWeaponImpactSoundController.cs
+++ Assets/Scripts/Audio/RangedWeaponImpactSoundController.cs
@@ -6,11 +6,31 @@ namespace Alxtrkhv.AudioSystem
     {
         [Header("References")]
         [SerializeField]
-        private SoundEventEmitter emitter;
+        private SoundEventEmitter playerEmitter;
+
+        [SerializeField]
+        private SoundEventEmitter allyEmitter;
+
+        [SerializeField]
+        private SoundEventEmitter enemyEmitter;
 
         public void PlayImpactSound(Vector3 position, UnitSide unitSide, SurfaceType surfaceType)
         {
+            var emitter = GetEmitter(unitSide);
 
+            emitter.EmitAtLocalPosition(position, new SoundEventConfig((int)surfaceType));
+        }
+
+        private SoundEventEmitter GetEmitter(UnitSide unitSide)
+        {
+            switch (unitSide) {
+                case UnitSide.Player: return playerEmitter;
+                case UnitSide.Ally: return allyEmitter;
+                case UnitSide.Enemy: return enemyEmitter;
+            }
+
+            Debug.LogError($"{unitSide} UnitSide is not supported");
+            return null;
         }
     }
 }
